42faf49341f122454a503d5d672c2052a4903954
ino <87593133+inoUwU@users.noreply.github.com>
2025-01-19 23:29:30 +0900

    クライアントアプリケーションにアップロード速度測定機能を追加

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
@@ -24,4 +25,19 @@ func main() {
 	duration := time.Since(start).Seconds()
 	downloadSpeed := float64(byteDownloaded*8) / (1024 * 1024) / duration
 	fmt.Printf("Download speed: %.2f Mbps\n", downloadSpeed)
+
+	// アップロード速度測定
+	fmt.Print("Measuring upload speed...")
+	data := bytes.Repeat([]byte("A"), 1024*1024)
+	start = time.Now()
+	resp, err = http.Post(serverURL+"/upload", "application/octet-stream", bytes.NewReader(data))
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	duration = time.Since(start).Seconds()
+	uploadSpeed := float64(len(data)*8) / (1024 * 1024) / duration
+	fmt.Printf("Upload speed: %.2f Mbps\n", uploadSpeed)
 }
